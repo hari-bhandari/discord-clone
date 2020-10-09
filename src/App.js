d9d0a96ebf2413a179add2421eb0f032471e7ee1
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-09 12:33:35 +0100

    added sidebar top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from "./Components/Layout/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Sidebar/>
     </div>
   );
 }
--- src/Components/Layout/Sidebar/Sidebar.js
+++ src/Components/Layout/Sidebar/Sidebar.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import './Sidebar.css'
+import {ExpandMore} from "@material-ui/icons";
 const Sidebar = () => {
     return (
-        <div>
-            
+        <div className={"sidebar"}>
+            <div className="sidebar__top">
+                <h3>Discord</h3>
+                <ExpandMoreIcon/>
+            </div>
+            <div className="sidebar__channels">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
