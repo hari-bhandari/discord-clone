8c967d07d0aa2b71aa5c6fee0df4bb6e2a9e4c9b
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-10 10:16:47 +0100

    added Chat Header

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Chat.css'
+import ChatHeader from "./ChatHeader";
 const Chat = () => {
     return (
         <div className="chat">
-            <h2> I am the chat screen</h2>
+            <ChatHeader/>
         </div>
     );
 };
--- src/Components/Chat/ChatHeader.js
+++ src/Components/Chat/ChatHeader.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import './ChatHeader.css'
 const ChatHeader = () => {
     return (
-        <div>
-
+        <div className="chatHeader">
+          <div className="chatHeader__left">
+            <h3><span className="chatHeader__hash">#</span>Test Channel</h3>
+          </div>
+            <div className="chatHeader__right">
+                <NotificationsIcon/>
+                <EditLocationRoundedIcon/>
+                <PeopleAltRoundedIcon/>
+                <div className="chatHeader__search">
+                    <input type="text" placeholder="Search"/>
+                    <SearchRoundedIcon/>
+                </div>
+            </div>
         </div>
     );
 };
