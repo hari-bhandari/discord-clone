6d2391ec5c704d422c6c6cdef561319c375c9372
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-27 20:32:13 +0000

    added redux bp

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/counter/userSlice';
+import userReducer from '../features/userSlice';
+import appReducer from '../features/appSlice';
 
 export default configureStore({
   reducer: {
-    counter: userReducer,
+    user: userReducer,
+    app: appReducer,
   },
 });
