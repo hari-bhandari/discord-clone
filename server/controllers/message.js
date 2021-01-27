f3c029fd313d577238a2a61590c4bf9648ec526a
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-27 09:56:00 +0000

    remeoved server to add firebase

--- server/controllers/message.js
+++
@@ -1,4 +0,0 @@
-import User from '../models/User.js'
-import Message from '../models/Message'
-import asyncHandler from "../middlewares/async.js";
-import ErrorResponse from "../utils/errorResponse.js";
--- server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import {register,login,getMe} from '../controllers/auth.js'
-const router=express.Router();
-router.route('/register').post(register);
-router.route('/login').post(login);
-router.route('/me').get(getMe)
-export default router
