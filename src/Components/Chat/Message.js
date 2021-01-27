fa4bc317ec2bcacbaafd3b40db5e2c2f8c376717
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-27 20:53:33 +0000

    used profile picture instead of hardcoded url

--- src/Components/Chat/Message.js
+++ src/Components/Chat/Message.js
@@ -5,7 +5,7 @@ import { Avatar } from '@material-ui/core';
 function Message({ timestamp, user, message }) {
     return (
         <div className="message">
-            <Avatar src="https://media-exp1.licdn.com/dms/image/C4D03AQE3y9f9rBa6YA/profile-displayphoto-shrink_400_400/0?e=1608163200&v=beta&t=A7JW7L3i6ZNiBOthnVrvJOR_dPBm4iXiC5DYxkd-pck" />
+            <Avatar src={user.photo} />
             <div className="message__info">
                 <h4> {user?.displayName}
                     <span className="message__timestamp">{new Date(timestamp?. toDate().toUTCString())}</span>
